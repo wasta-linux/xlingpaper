90018e7551c7cf7eb7f921e3e22fdfac6cd7a66b
Nate Marti <nate_marti@sil.org>
2021-11-10 20:52:08 +0100

    put EULA with other files

--- scripts/xlingpaper-wrapper.sh
+++ scripts/xlingpaper-wrapper.sh
@@ -8,7 +8,7 @@
 
 xxe_dir="${SNAP}/opt/xxe-perso-8_2_0"
 xxe_eula_name='XXEPersonalAndXLingPaperLicense'
-xxe_eula_file="${SNAP}/${xxe_eula_name}.txt"
+xxe_eula_file="${xxe_dir}/${xxe_eula_name}.txt"
 
 # Get EULA text; replace NBSP with normal space.
 # xxe_eula_text=$(sed -e 's/'$(echo "\xa0")'/ /g' "$xxe_ula_file")
